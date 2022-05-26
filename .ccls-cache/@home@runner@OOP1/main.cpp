a6120d82c337174fe883cf5958a5633a47347fa2
Mingmanas Sivaraksa <62439174+AjMing@users.noreply.github.com>
2022-05-26 01:36:14 +0000

    new pointer

--- .ccls-cache/@home@runner@OOP1/main.cpp
+++ .ccls-cache/@home@runner@OOP1/main.cpp
@@ -1,21 +1,41 @@
 #include <iostream>
 using namespace std;
+
 #include "student.h"
 
 int main() {
-  student s1,s2;
+/*  student s1,s2;
 
  // s1.name="PUN";  // ERROR, it is in private
-  s1.set_name("Pun");  	s1.set_age(20); 
+    s1.set_name("Pun");  	s1.set_age(20); 
 		s2.set_name("Joe");
 		s2.set_age(27); 
 		
     s1.get_age()>s2.get_age() ?
     s1.print_name(): s2.print_name(); 
-  /* if( s1.get_age()>s2.get_age() )
+   if( s1.get_age()>s2.get_age() )
      s1.print_name();
-       else  s2.print_name(); */
+       else  s2.print_name(); 
       cout <<"is older"<<endl;
-
-
+*/
+     student Cst[10],*p;
+     int i;
+     string n;
+     for(i=0;i<3;i++){
+         cout<<"What is your name?";
+         cin>>n;
+         Cst[i].set_name(n);
+     }
+    for(i=0;i<3;i++){
+       
+         Cst[i].print_name();
+     }
+    cout<<endl;
+    cout<<"Pointer"<<endl;
+    p=&Cst[2];
+    (*p).print_name();
+    p->print_name();
+    (p-1)->print_name();
 }
+
+//CST --> Joe  Jim Jan*
--- student.h
+++ student.h
@@ -1,5 +1,7 @@
 #ifndef student_h
 #define student_h
+#include <iostream>
+using namespace std;
 /* In the C Programming Language, the #ifndef directive allows for conditional compilation. The preprocessor determines if the provided macro does not exist before including the subsequent code in the compilation process.
 ref:https://www.techonthenet.com/c_language/directives/ifndef.php
 */
@@ -7,18 +9,24 @@ ref:https://www.techonthenet.com/c_language/directives/ifndef.php
 class student{
    private: string name;
             int age;
+            float score;
    public: 
         void set_name(string);
 		    void print_name();
         void set_age(int);
         int get_age();
+        void set_float(float s){s>100?score=100:score=s;  }
+        float get_float(){ return score;}
+       // void set_all(string,int,float);
 };
 
 
 void student::print_name(){
  		cout<<"Name:"<<name<<endl;
 }
-void student::set_name(string n){ name=n;}
+void student::set_name(string n){
+  name=n;
+}
 int student::get_age(){ return age;}
 
 void student::set_age(int n){ age=n;}
